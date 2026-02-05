2208345783d8b0df6ef71ab7aaa594cbe707d8c2
ejajlemon-11725 <ejajahmed725@gmail.com>
2026-02-05 20:28:48 +0600

    Initial Django project upload

--- vaxsafe_app/settings.py
+++ vaxsafe_app/settings.py
@@ -119,18 +119,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Email configuration (for OTP)
-# Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'katsukiaimeee@gmail.com'
-EMAIL_HOST_PASSWORD = 'kjtr kkcx loml ophu'  # Gmail App Password
-
-# settings.py
-AUTHORITY_EMAIL = 'katsukiaimeee@gmail.com'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
